2dc2754b5ab26396344864b1af2bdfd958f35364
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-05-15 12:00:32 +0530

    Initial Commit

---
+++ BBLoader/BBLoader.h
@@ -0,0 +1,19 @@
+//
+//  BBLoader.h
+//  BBLoader
+//
+//  Created by Bibin Jacob Pulickal on 15/05/20.
+//  Copyright © 2020 bibinjacobpulickal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BBLoader.
+FOUNDATION_EXPORT double BBLoaderVersionNumber;
+
+//! Project version string for BBLoader.
+FOUNDATION_EXPORT const unsigned char BBLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBLoader/PublicHeader.h>
+
+
