8afcc1b660cd46433381c359117dabd098075912
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-05-15 15:03:02 +0530

    Added BBLoader with convenience initializer.

--- Tests/BBLoaderTests/BBLoaderTests.swift
+++ Tests/BBLoaderTests/BBLoaderTests.swift
@@ -6,7 +6,7 @@ final class BBLoaderTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(BBLoader().text, "Hello, World!")
+        XCTAssertEqual("Hello, World!", "Hello, World!")
     }
 
     static var allTests = [
