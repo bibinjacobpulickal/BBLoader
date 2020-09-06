9f456ac3b9b4df0932dbfb5f9f4c71b6358e874b
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-09-06 14:37:11 +0530

    Updated usage of BBAlert usage in BBLoader.

--- Sources/BBLoader/BBLoaderPresentable.swift
+++ Sources/BBLoader/BBLoaderPresentable.swift
@@ -1,38 +1,26 @@
 import UIKit
+import BBAlert
 
-public protocol BBLoaderPresentable {
+extension BBAlertPresentable {
 
-    func present(_ viewControllerToPresent: UIViewController,
-                 completion: (() -> Void)?)
-    func present(_ viewControllerToPresent: UIViewController,
-                 animated flag: Bool,
-                 completion: (() -> Void)?)
-}
+    public func presentBBLoader(duration: Double = 0,
+                                setup: ((BBLoader) -> Void)? = nil,
+                                completion: ((BBLoader) -> Void)? = nil) {
 
-extension UIViewController: BBLoaderPresentable { }
-
-extension BBLoaderPresentable {
-
-    public func present(_ viewControllerToPresent: UIViewController, completion: (() -> Void)? = nil) {
-        present(viewControllerToPresent, animated: true, completion: completion)
-    }
-}
-
-extension BBLoaderPresentable {
-
-    public func presentBBLoader(duration: Double = 0, setup: ((BBLoader) -> Void)? = nil, completion: ((BBLoader) -> Void)? = nil) {
-
-        let loader = BBLoader(title: "Loading", message: "Please wait...")
+        let loader = BBLoader(title: "Loading",
+                              message: "Please wait...")
         setup?(loader)
 
         present(loader) {
             completion?(loader)
 
-            self.handleDismissal(loader, after: duration)
+            self.handleDismissal(loader,
+                                 after: duration)
         }
     }
 
-    private func handleDismissal(_ loader: BBLoader, after duration: Double) -> Void {
+    private func handleDismissal(_ loader: BBLoader,
+                                 after duration: Double) -> Void {
 
         if !duration.isZero {
             DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
