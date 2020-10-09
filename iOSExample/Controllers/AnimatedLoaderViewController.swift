eb58f9197a8e9f0009bc985dbd0cb3f9a4a2ab19
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-10-09 19:58:00 +0530

    Modified animated loader sample.

--- iOSExample/Controllers/AnimatedLoaderViewController.swift
+++ iOSExample/Controllers/AnimatedLoaderViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import BBLoader
 
-class AnimatedLoaderViewController: UIViewController {
+class AnimatedLoaderViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,9 +19,10 @@ class AnimatedLoaderViewController: UIViewController {
     }
 
     @objc func presentAnimatedLoader() {
-        presentBBAnimatedLoader(type: .quadRotatingSquaresToCircles, duration: 4, setup: { loader in
+        presentBBAnimatedLoader(type: .quadRotatingSquaresToCircles, setup: { loader in
             guard let loader = loader as? BBQuadRotatingSquaresToCircles else { return }
             loader.animationCycleDuration = 0.5
+            loader.sizeWidthMultiplier    = 0.4
             loader.animatingColor         = #colorLiteral(red: 0.5502940416, green: 0.8689419627, blue: 0.9688422084, alpha: 1)
             loader.animatingColors        = ((#colorLiteral(red: 0.5502940416, green: 0.8689419627, blue: 0.9688422084, alpha: 1), #colorLiteral(red: 0.9374079108, green: 0.7400206923, blue: 1, alpha: 1)), (#colorLiteral(red: 0.6756847501, green: 0.7432909608, blue: 0.9375373721, alpha: 1), #colorLiteral(red: 0.9669341445, green: 0.646687448, blue: 0.7728756666, alpha: 1)))
             loader.startAngle             = .zero
