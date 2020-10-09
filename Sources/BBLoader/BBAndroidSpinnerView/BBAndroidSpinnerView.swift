f68032633a1325c21cc68163f2cdb8b20b84692c
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-10-10 01:04:40 +0530

    Added new BBAndroidSpinnerView.

---
+++ Sources/BBLoader/BBAndroidSpinnerView/BBAndroidSpinnerView.swift
@@ -0,0 +1,162 @@
+//
+//  BBAndroidSpinnerView.swift
+//
+//  Copyright © 2020 Bibin Jacob Pulickal (https://github.com/bibinjacobpulickal)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import UIKit
+
+@IBDesignable
+class BBAndroidSpinnerView: UIView {
+
+    /// Animating line width. Defaults to 4.
+    public var lineWidth = 4
+
+    /// Animating shape size, by default size determined by ratio of width. Defaults to zero.
+    public var animationSize = CGSize.zero
+
+    /// Animating shape size is determined with respect the ratio to width of presenting view.
+    public var sizeWidthMultiplier: CGFloat = 0.5
+
+    override var layer: CAShapeLayer {
+        get {
+            return super.layer as! CAShapeLayer
+        }
+    }
+
+    override class var layerClass: AnyClass {
+        return CAShapeLayer.self
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.fillColor = nil
+        layer.strokeColor = UIColor.black.cgColor
+        layer.lineWidth = lineWidth
+        setPath()
+    }
+
+    override func didMoveToWindow() {
+        startAnimating()
+    }
+
+    private func setPath() {
+        layer.path = UIBezierPath(ovalIn: bounds.insetBy(dx: layer.lineWidth / 2, dy: layer.lineWidth / 2)).cgPath
+    }
+
+    struct Pose {
+        let secondsSincePriorPose: CFTimeInterval
+        let start: CGFloat
+        let length: CGFloat
+        init(_ secondsSincePriorPose: CFTimeInterval, _ start: CGFloat, _ length: CGFloat) {
+            self.secondsSincePriorPose = secondsSincePriorPose
+            self.start = start
+            self.length = length
+        }
+    }
+
+    class var poses: [Pose] {
+        get {
+            return [
+                Pose(0.0, 0.000, 0.7),
+                Pose(0.6, 0.500, 0.5),
+                Pose(0.6, 1.000, 0.3),
+                Pose(0.6, 1.500, 0.1),
+                Pose(0.2, 1.875, 0.1),
+                Pose(0.2, 2.250, 0.3),
+                Pose(0.2, 2.625, 0.5),
+                Pose(0.2, 3.000, 0.7),
+            ]
+        }
+    }
+}
+
+extension BBAndroidSpinnerView: BBAnimatable {
+
+    func startAnimating() {
+
+        guard let superview = superview else { return }
+        if animationSize == .zero {
+            NSLayoutConstraint.activate([
+                widthAnchor.constraint(equalTo: superview.widthAnchor, multiplier: sizeWidthMultiplier),
+                heightAnchor.constraint(equalTo: widthAnchor)
+            ])
+        } else {
+            NSLayoutConstraint.activate([
+                widthAnchor.constraint(equalToConstant: animationSize.width),
+                heightAnchor.constraint(equalToConstant: animationSize.height)
+            ])
+        }
+
+        var time: CFTimeInterval = 0
+        var times = [CFTimeInterval]()
+        var start: CGFloat = 0
+        var rotations = [CGFloat]()
+        var strokeEnds = [CGFloat]()
+
+        let poses = type(of: self).poses
+        let totalSeconds = poses.reduce(0) { $0 + $1.secondsSincePriorPose }
+
+        for pose in poses {
+            time += pose.secondsSincePriorPose
+            times.append(time / totalSeconds)
+            start = pose.start
+            rotations.append(start * 2 * .pi)
+            strokeEnds.append(pose.length)
+        }
+
+        times.append(times.last!)
+        rotations.append(rotations[0])
+        strokeEnds.append(strokeEnds[0])
+
+        animateKeyPath(keyPath: "strokeEnd", duration: totalSeconds, times: times, values: strokeEnds)
+        animateKeyPath(keyPath: "transform.rotation", duration: totalSeconds, times: times, values: rotations)
+
+        animateStrokeHueWithDuration(duration: totalSeconds * 5)
+    }
+
+    func animateKeyPath(keyPath: String, duration: CFTimeInterval, times: [CFTimeInterval], values: [CGFloat]) {
+        let animation = CAKeyframeAnimation(keyPath: keyPath)
+        animation.keyTimes = times as [NSNumber]?
+        animation.values = values
+        animation.calculationMode = .linear
+        animation.duration = duration
+        animation.repeatCount = Float.infinity
+        layer.add(animation, forKey: animation.keyPath)
+    }
+
+    func animateStrokeHueWithDuration(duration: CFTimeInterval) {
+        let count = 36
+        let animation = CAKeyframeAnimation(keyPath: "strokeColor")
+        animation.keyTimes = (0 ... count).map { NSNumber(value: CFTimeInterval($0) / CFTimeInterval(count)) }
+        animation.values = (0 ... count).map {
+            UIColor(hue: CGFloat($0) / CGFloat(count), saturation: 1, brightness: 1, alpha: 1).cgColor
+        }
+        animation.duration = duration
+        animation.calculationMode = .linear
+        animation.repeatCount = Float.infinity
+        layer.add(animation, forKey: animation.keyPath)
+    }
+
+    func stopAnimating() {
+        removeFromSuperview()
+    }
+}
